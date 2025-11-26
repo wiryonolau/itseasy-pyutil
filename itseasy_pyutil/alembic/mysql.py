f362d81354e6fba84c49504aeef80948d9b3ed49
Wiryono Lauw <wiryonolau@gmail.com>
2025-11-26 17:43:42 +0700

    alembic mysql support enum

--- itseasy_pyutil/alembic/mysql.py
+++ itseasy_pyutil/alembic/mysql.py
@@ -38,6 +38,7 @@ def normalize_column_type(mysql_type):
         "time": "Time",
         "tinyint": "Boolean",
         "varchar": "String",
+        "enum": "Enum",
     }
     return type_mapping.get(mysql_type, mysql_type.upper())
 
@@ -58,6 +59,7 @@ def normalize_sa_type(sa_type):
         "Time": "time",
         "JSON": "longtext",
         "LargeBinary": "blob",
+        "Enum": "enum",
     }
 
     return type_mapping.get(sa_type, sa_type.lower())
@@ -100,8 +102,12 @@ def column_info(connection, table_name, column_name):
     query = text(
         """
         SELECT 
-            COLUMN_DEFAULT, IS_NULLABLE, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH, 
-            NUMERIC_PRECISION, NUMERIC_SCALE, EXTRA,
+            COLUMN_DEFAULT, COLUMN_TYPE, 
+            IS_NULLABLE, DATA_TYPE,
+            CHARACTER_MAXIMUM_LENGTH, 
+            NUMERIC_PRECISION, 
+            NUMERIC_SCALE, 
+            EXTRA,
             CASE
                 WHEN COLUMN_TYPE LIKE '%unsigned%' THEN TRUE
                 ELSE FALSE 
@@ -122,6 +128,7 @@ def column_info(connection, table_name, column_name):
     if result:
         (
             col_default,
+            col_type,
             is_nullable,
             data_type,
             char_length,
@@ -137,6 +144,7 @@ def column_info(connection, table_name, column_name):
 
         return (
             data_type,
+            col_type,
             is_nullable.lower(),
             normalize_default_value(col_default),
             char_length if char_length is not None else None,
@@ -191,6 +199,7 @@ def remove_old_columns(connection, metadata):
 def column_has_update(existing, new):
     (
         existing_type,
+        existing_col_type,
         existing_nullable,
         existing_default,
         existing_length,
@@ -202,6 +211,7 @@ def column_has_update(existing, new):
 
     (
         new_type,
+        new_col_type,
         new_nullable,
         new_default,
         new_length,
@@ -247,6 +257,9 @@ def column_has_update(existing, new):
     if new_scale is not None and existing_scale != new_scale:
         return True
 
+    if new_type == "enum" and existing_col_type.lower() != new_col_type.lower():
+        return True
+
     for k, v in existing_erratas.items():
         if new_erratas.get(k, None) != v:
             return True
@@ -259,6 +272,13 @@ def modify_column_if_needed(connection, table_name, column_name, column_obj):
     existing_info = column_info(connection, table_name, column_name)
 
     new_type = type(column_obj.type).__name__
+
+    new_col_type = new_type
+    if new_type.lower() == "enum":
+        enum_class = getattr(column_obj.type, "enum_class")
+        enum_name = ["'{}'".format(e.name) for e in enum_class]
+        new_col_type = "enum({})".format(",".join(enum_name))
+
     new_nullable = "yes" if column_obj.nullable else "no"
     new_default = normalize_default_value(
         column_obj.default or column_obj.server_default
@@ -273,6 +293,7 @@ def modify_column_if_needed(connection, table_name, column_name, column_obj):
 
     new_info = (
         new_type,
+        new_col_type,
         new_nullable,
         new_default,
         new_length,
@@ -285,10 +306,14 @@ def modify_column_if_needed(connection, table_name, column_name, column_obj):
     if existing_info:
         if column_has_update(existing_info, new_info):
             print(
-                f"Checking {table_name}.{column_name} -> Existing: {existing_info}, New: {new_info}"
+                f"Checking {column_name} in {table_name} -> Existing: {existing_info}, New: {new_info}"
             )
             new_type = normalize_sa_type(new_type)
-            alter_stmt = f"ALTER TABLE `{table_name}` MODIFY COLUMN `{column_name}` {new_type}"
+
+            if new_type == "enum":
+                alter_stmt = f"ALTER TABLE `{table_name}` MODIFY COLUMN `{column_name}` {new_col_type}"
+            else:
+                alter_stmt = f"ALTER TABLE `{table_name}` MODIFY COLUMN `{column_name}` {new_type}"
         else:
             print(f"Skipping {table_name}.{column_name} : No changes detected.")
             return
@@ -297,15 +322,16 @@ def modify_column_if_needed(connection, table_name, column_name, column_obj):
             f"Column {column_name} does not exist in {table_name}. Creating column."
         )
         new_type = normalize_sa_type(new_type)
-        alter_stmt = (
-            f"ALTER TABLE `{table_name}` ADD COLUMN `{column_name}` {new_type}"
-        )
 
-    if new_length is not None:
+        if new_type == "enum":
+            alter_stmt = f"ALTER TABLE `{table_name}` ADD COLUMN `{column_name}` {new_col_type}"
+        else:
+            alter_stmt = f"ALTER TABLE `{table_name}` ADD COLUMN `{column_name}` {new_type}"
+
+    if new_type != "enum" and new_length is not None:
         alter_stmt += f"({new_length})"
     elif new_precision is not None and new_scale is not None:
         alter_stmt += f"({new_precision}, {new_scale})"
-
     if new_is_unsigned:
         alter_stmt += f" UNSIGNED"
     if new_erratas.get("auto_increment"):
